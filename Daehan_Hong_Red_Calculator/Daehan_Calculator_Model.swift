4344d439cd660e9e543dbe6b609b9f5461c4a081
Daehan Hong <200307118@ba-a251-2318.student.georgianc.on.ca>
2019-11-06 19:29:43 -0500

    first commit

---
+++ Daehan_Hong_Red_Calculator/Daehan_Calculator_Model.swift
@@ -0,0 +1,9 @@
+//
+//  Daehan_Calculator_Model.swift
+//  Daehan_Hong_Red_Calculator
+//
+//  Created by Daehan Hong on 2019-09-11.
+//  Copyright © 2019 Daehan Hong. All rights reserved.
+//
+
+import Foundation
